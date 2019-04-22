63f04f08f6b667bd82da01bf3099c67a1bf67d02
DsWork <249725997@qq.com>
2019-04-22 12:00:27 +0800

    删除原UniqueId方法，为新的id规则类IdUtil

--- DsWorkJava/dswork-core/dswork/core/util/IdUtil.java
+++ DsWorkJava/dswork-core/dswork/core/util/IdUtil.java
@@ -5,7 +5,7 @@ package dswork.core.util;
  */
 public class IdUtil
 {
-	private static long getDatacenterId(long maxDatacenterId)
+	private static long getIdDatacenter(long maxDatacenterId)
 	{
 		long id = 0L;
 		try
@@ -32,7 +32,7 @@ public class IdUtil
 		return id;
 	}
 
-	private static long getMaxWorkerId(long datacenterId, long maxWorkerId)
+	private static long getIdWorker(long datacenterId, long maxWorkerId)
 	{
 		StringBuilder mpid = new StringBuilder();
 		mpid.append(datacenterId);
@@ -44,20 +44,18 @@ public class IdUtil
 		return (mpid.toString().hashCode() & 0xffff) % (maxWorkerId + 1);// MAC + PID 的 hashcode 获取16个低位
 	}
 	
-	private final static long datacenterIdLength = 5L;// 数据标识所占的2进制位数
-	private final static long workerIdLength = 5L;// 机器码所占的2进制位数
-	private final static long sequenceLength = 8L;// 序列号所占的2进制位数
-	private final static long workerIdShift = sequenceLength;
-	private final static long datacenterIdShift = sequenceLength + workerIdLength;// 数据标识向左移13位(5+8)
-	private final static long timestampShift = sequenceLength + workerIdLength + datacenterIdLength;// 时间截向左移18位(5+5+8)
-	private final static long sequenceMask = -1L ^ (-1L << sequenceLength);// 生成序列的掩码，这里为255
-	private final static long maxDatacenterId = -1L ^ (-1L << datacenterIdLength);// 数据标识的long值，5位是31
-	private final static long maxWorkerId = -1L ^ (-1L << workerIdLength);// 机器码的long值，5位是31
-
-	private static long datacenterId = getDatacenterId(maxDatacenterId);// 数据标识
-	private static long workerId = getMaxWorkerId(datacenterId, maxWorkerId);// 数据标识
-	private static long sequence = 0L;// 毫秒内序列号
-	private static long lastTimeMillis = -1L;// 上次生成ID的时间截
+	private final static long idDatacenterLength = 5L;// 数据标识所占的2进制位数
+	private final static long idWorkerLength     = 5L;// 机器码所占的2进制位数
+	private final static long maxIdDatacenter   = -1L ^ (-1L << idDatacenterLength);// 数据标识的long值，5位是31
+	private final static long maxIdWorker        = -1L ^ (-1L << idWorkerLength);// 机器码的long值，5位是31
+	private final static long datacenterId = getIdDatacenter(maxIdDatacenter);// 数据标识
+	private final static long workerId = getIdWorker(datacenterId, maxIdWorker);// 数据标识
+	
+	
+	// 用于生成16位id
+	private final static long datacenterIdShift = idWorkerLength;// 数据标识向左移5位(5)
+	private final static long timestampShift    = idWorkerLength + idDatacenterLength;// 时间截向左移10位(5+5)
+	private       static long lastTimeMillis = -1L;// 上次生成ID的时间截
 
 	private static long timeGen()
 	{
@@ -75,7 +73,52 @@ public class IdUtil
 	}
 
 	/**
-	 * 根据时间戳和机器信息生产一个唯一的18位ID，具有防止重复机制，但2090-11-18 22:07:45.624后的ID将超出18位<br>
+	 * 根据时间戳和机器信息生产一个唯一的16位ID，具有防止重复机制<br>
+	 * 结构：正数0 - 时间戳 - 10位机器码 <br>
+	 * 长型形共64位，最高位是符号位，0正数，1负数：<br>
+	 * 0 - 000 - 0000000000 0000000000 0000000000 0000000000 0000000000 - 1111111111<br>
+	 * javascript中不会计算出问题的长整形最大值为9007199254740992，取二进制53位全1，即9007199254740991<br>
+	 * 0 - 000 00 - 000001 11 1111111111 1111111111 1111111111 1111111111 - 1111111111<br>
+	 * (Long.parseLong("1111111111111111111111111111111111111111111", 2)) = 8796093022207L<br>
+	 * 2248-09-26 23:10:22.207后的id将超出js可计算长度
+	 * 10位的数据机器位，可以部署在1024个节点<br>
+	 * @return long 最大16位的id
+	 */
+	public synchronized static long genId()
+	{
+		long times = timeGen();
+		if(lastTimeMillis == times)
+		{
+			times = nextMillis(lastTimeMillis);// 毫秒内序列溢出，阻塞到下一个毫秒,获得新的时间戳
+		}
+		lastTimeMillis = times;// 上次生成ID的时间截
+		return (times << timestampShift) //
+				| (datacenterId << datacenterIdShift) //
+				| workerId;// 移位并通过或运算拼到一起组成ID
+	}
+
+	/**
+	 * 根据genId()获取到的id转化为java时间截
+	 * @param id genId()获取到的id
+	 * @return long java时间截
+	 */
+	public static long formatId(long id)
+	{
+		return id >> 10;
+	}
+	
+
+	// 用于生成18位id
+	private final static long     idSequenceLength    = 8L;// 序列号所占的2进制位数
+	private final static long UniqueIdWorkerShift     = idSequenceLength;
+	private final static long UniqueIdDatacenterShift = idSequenceLength + idWorkerLength;// 数据标识向左移13位(5+8)
+	private final static long UniqueTimestampShift    = idSequenceLength + idWorkerLength + idDatacenterLength;// 时间截向左移18位(5+5+8)
+	private final static long UniqueSequenceMask      = -1L ^ (-1L << idSequenceLength);// 生成序列的掩码，这里为255
+	private       static long UniqueLastTimeMillis    = -1L;// 上次生成ID的时间截
+	private       static long sequence                = 0L;// 毫秒内序列号
+
+	/**
+	 * 根据时间戳和机器信息生产一个唯一的18位ID，javascript不适用，需转String，具有防止重复机制，但2090-11-18 22:07:45.624后的ID将超出18位<br>
 	 * 结构：正数0 - 时间戳 - 10位机器码 - 8位序列号<br>
 	 * 长型形共64位，最高位是符号位，0正数，1负数：<br>
 	 * 0 - 000 - 00 0000000000 0000000000 0000000000 0000000000 - 1111111111 - 11111111<br>
@@ -86,39 +129,40 @@ public class IdUtil
 	 * 8位序列号可支持每个节点每毫秒(同一机器，同一时间截)产生256个ID序号
 	 * @return long 最大18位的id
 	 */
-	public synchronized static long genId()
+	public synchronized static long genUniqueId()
 	{
 		long times = timeGen();
 		// if(timestamp < lastTimestamp){}// 系统时钟回退过
-		if(lastTimeMillis == times)
+		if(UniqueLastTimeMillis == times)
 		{
-			sequence = (sequence + 1) & sequenceMask;// 如果是同一时间生成的，则进行毫秒内序列
+			sequence = (sequence + 1) & UniqueSequenceMask;// 如果是同一时间生成的，则进行毫秒内序列
 			if(sequence == 0)
 			{
-				times = nextMillis(lastTimeMillis);// 毫秒内序列溢出，阻塞到下一个毫秒,获得新的时间戳
+				times = nextMillis(UniqueLastTimeMillis);// 毫秒内序列溢出，阻塞到下一个毫秒,获得新的时间戳
 			}
 		}
 		else
 		{
 			sequence = 0L;// 时间戳改变，毫秒内序列重置
 		}
-		lastTimeMillis = times;// 上次生成ID的时间截
-		return (times << timestampShift) //
-				| (datacenterId << datacenterIdShift) //
-				| (workerId << workerIdShift) //
+		UniqueLastTimeMillis = times;// 上次生成ID的时间截
+		return (times << UniqueTimestampShift) //
+				| (datacenterId << UniqueIdDatacenterShift) //
+				| (workerId << UniqueIdWorkerShift) //
 				| sequence;// 移位并通过或运算拼到一起组成64位的ID
 	}
 
 	/**
-	 * 根据genId()获取到的id转化为java时间截
+	 * 根据genUniqueId()获取到的id转化为java时间截
 	 * @param id genId()获取到的id
 	 * @return long java时间截
 	 */
-	public static long formatId(long id)
+	public static long formatUniqueId(long id)
 	{
-		return id >> 18;
+		return id >> 10;
 	}
-	
+
+	private static long lastTimeId = -1L;// 上次生成ID的时间截
 	/**
 	 * 根据时间戳产生一个唯一ID，具有防止重复机制
 	 * @return long
@@ -128,10 +172,10 @@ public class IdUtil
 		long id = 0;
 		do
 		{
-			id = java.util.Calendar.getInstance().getTimeInMillis();
+			id = timeGen();
 		}
-		while(id == lastTimeMillis);
-		lastTimeMillis = id;
+		while(id == lastTimeId);
+		lastTimeId = id;
 		return id;
 	}
 }
